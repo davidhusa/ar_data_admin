fdd81bddda94b3585cb91660a74ba8437be261ba
David Husa <davidhusa@gmail.com>
2013-01-14 19:46:07 -0800

    first commit, ar working

---
+++ app/models/post.rb
@@ -0,0 +1,3 @@
+class Post < ActiveResource::Base
+	self.site = 'http://localhost:3000/'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArDataAdmin::Application.config.secret_token = '07cf17a3585dba9658b524fc050ddeb05dfb6ff9c4f3adbb867d9a8dd991e54127f099572e4fbd45deb3b3b4ae06a08fcdb5e644d15e4953c1062ce23939878b'
